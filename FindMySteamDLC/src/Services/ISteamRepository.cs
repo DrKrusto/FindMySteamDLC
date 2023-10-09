fbd5c89bc1b17ba8b55eaadee05617fe0e06b843
Evan <evan.roussin@gmail.com>
2023-10-10 00:15:50 +0200

    Created the SteamRepository service

---
+++ FindMySteamDLC/src/Services/ISteamRepository.cs
@@ -0,0 +1,21 @@
+﻿using FindMySteamDLC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMySteamDLC.Services
+{
+    public interface ISteamRepository
+    {
+        public void AddGame(Game game);
+        public void AddDlc(Dlc dlc);
+        public void AddDlcs(IEnumerable<Dlc> dlcs);
+        public void AddGames(IEnumerable<Game> games);
+        public Game GetGame(int appID);
+        public Dlc GetDlc(int appID);
+        public IEnumerable<Game> GetGames();
+        public IEnumerable<Dlc> GetDlcsForGame(int appID);
+    }
+}
---
+++ FindMySteamDLC/src/Services/SteamRepository.cs
@@ -0,0 +1,82 @@
+﻿using FindMySteamDLC.Data;
+using FindMySteamDLC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMySteamDLC.Services
+{
+    public class SteamRepository : ISteamRepository
+    {
+        private readonly SteamDbContext context;
+        private readonly ISteamService steamService;
+
+        public SteamRepository(SteamDbContext context, ISteamService steamService)
+        {
+            this.context = context;
+            this.steamService = steamService;
+        }
+
+        public async void AddAppsFromFiles(string pathToSteam)
+        {
+            var games = await steamService.GetGamesFromFiles(pathToSteam);
+            var dlcs = games.SelectMany(game => game.Dlcs);
+
+            this.AddGames(games);
+            this.AddDlcs(dlcs);
+        }
+
+        public void AddDlc(Dlc dlc)
+        {
+            this.context.Add(dlc);
+            this.context.SaveChanges();
+        }
+
+        public void AddDlcs(IEnumerable<Dlc> dlcs)
+        {
+            this.context.AddRange(dlcs);
+            this.context.SaveChanges();
+        }
+
+        public void AddGame(Game game)
+        {
+            this.context.Add(game);
+            this.context.SaveChanges();
+        }
+
+        public void AddGames(IEnumerable<Game> games)
+        {
+            this.context.AddRange(games);
+            this.context.SaveChanges();
+        }
+
+        public Dlc GetDlc(int appID)
+        {
+            return this.context
+                .Dlcs
+                .FirstOrDefault(dlc => dlc.AppID == appID);
+        }
+
+        public IEnumerable<Dlc> GetDlcsForGame(int appID)
+        {
+            return this.context
+                .Dlcs
+                .Where(dlc => dlc.Game.AppID == appID)
+                .ToList();
+        }
+
+        public Game GetGame(int appID)
+        {
+            return this.context
+                .Games
+                .FirstOrDefault(game => game.AppID == appID);
+        }
+
+        public IEnumerable<Game> GetGames()
+        {
+            return this.context.Games.ToList();
+        }
+    }
+}
