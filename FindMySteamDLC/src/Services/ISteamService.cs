97f2c68e01d8fcb5aca163c4413d5e652a3d5627
Evan <evan.roussin@gmail.com>
2023-10-09 03:12:24 +0200

    Creating SteamService to replace a part of SteamInfo

---
+++ FindMySteamDLC/src/Services/ISteamService.cs
@@ -0,0 +1,16 @@
+﻿using FindMySteamDLC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMySteamDLC.src.Services
+{
+    public interface ISteamService
+    {
+        public IEnumerable<Game> GetGamesFromFiles(string pathToSteam);
+        public bool GetDlcsFromFiles(int AppID);
+        public string GetSteamRepository();
+    }
+}
---
+++ FindMySteamDLC/src/Services/SteamService.cs
@@ -0,0 +1,47 @@
+﻿using FindMySteamDLC.Models;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMySteamDLC.src.Services
+{
+    public class SteamService : ISteamService
+    {
+        public SteamService()
+        {
+
+        }
+
+        public bool GetDlcsFromFiles(int AppID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Game> GetGamesFromFiles(string pathToSteam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSteamRepository()
+        {
+            // TODO: not hardcode the paths
+            List<string> paths = new List<string>
+            {
+                @"SOFTWARE\Valve\Steam",
+                @"SOFTWARE\Wow6432Node\Valve\Steam"
+            };
+
+            foreach (string path in paths)
+            {
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(path);
+                if (key != null)
+                    return key.GetValue("InstallPath").ToString();
+            }
+
+            return null;
+        }
+    }
+}
